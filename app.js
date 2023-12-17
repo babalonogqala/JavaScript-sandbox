4e4c41782de2388f958f783baeb7dc3052854db2
Babalo <babalonogqala2003@gmail.com>
2023-12-17 12:13:52 +0200

    initial commit adding javscript code
    added how to use variables and arrays includding objects and learned to target them using alart function

---
+++ app.js
@@ -0,0 +1,22 @@
+// let greetingText = "Hi I'm Babalo!";
+// let number = 32;
+// alert(greetingText);
+// alert(greetingText);
+// greetingText = "Hi i am babalo nogqala";
+// alert(greetingText);
+
+// age = 100;
+// alert("I am " + age + " years of age no CAP");
+// alert(number);
+
+
+// variables
+let age = 20;
+let username = "Babalo";
+// arrays
+let hobbies = ["gaming", "cooking", "Reading"];
+// objects with its properties
+let job = {title:"developer", place:"cape town", salary:"R10000"};
+
+alert(hobbies [0]);
+alert(job.place)
