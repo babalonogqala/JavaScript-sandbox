617caace4d4ef5523f4faadb453f56051dc27245
Babalo <babalonogqala2003@gmail.com>
2023-12-19 17:30:44 +0200

    add practice with math operations and methods

---
+++ operations.js
@@ -0,0 +1,18 @@
+// math operations
+console.log(20 + 100);
+
+let answer;
+answer = 10 * 10;
+answer++;
+console.log(answer);
+
+ console.log("babalo " + "nogqala");
+
+
+ let userName = "Babalo";
+ console.log(userName.length);
+ console.log(userName.toUpperCase());
+
+ let course = ["web development", " android development", " react developer"];
+ console.log(course.length);
+ 
